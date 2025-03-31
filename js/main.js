9bc153fcd5319cccf168758a2b54facd7f03ffa5
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-03-31 13:39:47 -0400

    edits to about.css, main.css, main.js
    made header ID
    added header html and js
    added header styling to main.css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-// javascript code goes here //
+
+// header //
+
+// js + html code so we can use javascript to import header to each page
+// header styling done in main.css
+
+const headerGlobal = document.querySelector('#header-global');
+// to grab header ID from html
+
+headerGlobal.innerHTML = `
+<header class="header-global">
+    <nav class="header-navbar">
+        <div class="header-logo">Logo Placeholder</div>
+        <ul class="header-ul-nav-links">
+            <li class="header-li-nav-links"><a href="#">Home</a></li>
+            <li class="header-li-nav-links"><a href="#">About</a></li>
+            <li class="header-li-nav-links"><a href="#">Product</a></li>
+            <li class="header-li-nav-links"><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header> `;
+
+// footer //
